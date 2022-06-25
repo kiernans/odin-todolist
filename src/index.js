214d8500b61a4dba4ef5700b354151a4220910b1
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2022-06-24 18:57:42 -0700

    Sidebar button styling

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import Task from './task';
 import DOM from './dom';
 import './style.css';
 
+
+const addProjectListener = () => {
+    const button = document.querySelector('.add-project');
+    button.addEventListener('click', () => {
+        button.style.visibility = 'hidden';
+        
+    });
+};
+
 DOM.setup();
+addProjectListener();
